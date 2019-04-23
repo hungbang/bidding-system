e2a95ddbabef6cdd4f3b4ad2a02f564f87f9b7e6
hungbang <quanhungbang@gmail.com>
2019-04-23 10:39:11 +0700

    add kafka testing

---
+++ src/test/java/com/hbq/biddingsystem/SimpleKafkaTest.java
@@ -0,0 +1,13 @@
+package com.hbq.biddingsystem;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EmbeddedKafka
+public class SimpleKafkaTest {
+}
